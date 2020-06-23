0edab7aa6952cb2401346ba36b71f2d5a850d0f9
Nad304 <57240711+Nad304@users.noreply.github.com>
2020-06-23 02:41:03 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("button").onclick = function(){
+    document.getElementById("question").innerHTML = "Thank you.";
+    document.getElementById("button").style.display = "none";
+    document.getElementById("button2").style.display = "none";
+}
+document.getElementById("button2").onclick = function(){
+    document.getElementById("question").innerHTML = ":( fuk u ";
+    document.getElementById("button").style.display = "none";
+    document.getElementById("button2").style.display = "none";
+}
+
+//this is how you make a comment 
